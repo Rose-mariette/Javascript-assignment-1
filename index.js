a4850f52da4f8168591193c94b49e9aa4167c74b
ROSEMARIETTE <rosemariette23@gmail.com>
2025-08-13 08:14:05 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,93 @@
+// var x = 30;
+
+// const rose = "40";
+
+// let x = 10;
+
+// if (x > 5) {
+//     let y = 20;
+//     console.log(y);
+// }
+
+// consletole.log(y);
+
+// let check = 20 > 10;
+// console.log(check);
+
+// let number = null;
+// console.log("=======================");
+// console.log(number);
+// console.log("=======================");
+
+// function rose() {
+//   console.log("this is is a string");
+// }
+// let divide = 10 + 2;
+// console.log(divide);
+
+// let zzz = 10;
+// console.log(++zzz);
+
+// let c = 10;
+
+// let d = "10";
+
+// console.log(c == d);
+
+// let e = 10;
+
+// let f = 10;
+
+// console.log(e == f);
+
+// loop
+// for (let looping = 1;looping <= 10; looping++) {
+//     console.log("I am sorry");
+// }
+
+// for (let looping1 = 1; looping1 <= 100; looping1++) {
+//   console.log(looping1);
+// }
+
+//  coding = ["Javascript", "Python", "Java", "C++", "Ruby", "Typescript"];
+
+// for (let i = 0; i < coding.length; i++) {
+//   console.log(coding[i]);
+// }
+
+// function greet(firstName, lastName) {
+//   console.log("Hello" + firstName + " ", lastName);
+// }
+
+// greet("Rose", "Mariette");
+
+// function fn1(x) {
+//   function fn2(y) {
+//     return x + y;
+//   }
+//   return fn2;
+// }
+
+// let result = fn1(3);
+// console.log(result(2));
+
+// function add(a, b) {
+//   return a + b;
+// }
+// add(3, 5);
+
+// const person = {
+//   firstName: "Rose",
+//   lastName: "Mariette",
+//   age: 30,
+//   address: {
+//     street: "Ifite",
+//     city: "Awka",
+//     state: "Anambra",
+//     country: "Nigeria",
+//   },
+// };
+
+// // console.log(typeof person);
+// // console.log(person.lastName);
+// console.log(person.address.city);
