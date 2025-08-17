0a83fd3b1c9d26124aa53c253fd6504a6677d324
ROSEMARIETTE <rosemariette23@gmail.com>
2025-08-18 00:09:46 +0100

    fourth

---
+++ index.js
@@ -0,0 +1,66 @@
+// var x = 30;
+
+// const rose = "40";
+
+// let x = 10;
+
+// if (x > 5) {
+//     let y = 20;
+//     console.log(y);
+// }
+
+// console.log(y);
+
+
+let check = 20 > 10
+console.log(check);
+
+
+let number = null
+console.log('=======================');
+console.log(number);
+console.log('=======================');
+
+
+function rose() {
+    console.log ("this is is a string")
+}
+let divide = 10 + 2;
+console.log(divide);
+
+let zzz = 10
+console.log(++zzz);
+
+let c = 10
+
+let d = "10"
+
+console.log(c == d);
+
+let e = 10;
+
+let f = 10;
+
+console.log(e == f);
+
+
+// loop
+// for (let looping = 1;looping <= 10; looping++) {
+//     console.log("I am sorry"); 
+// }
+
+// for (let looping1 = 1; looping1 <= 100; looping1++) {
+//   console.log(looping1);
+// }
+
+let coding = ["Javascript", "Python", "Java", "C++", "Ruby", "Typescript"];
+
+for (let i = 0; i < coding.length; i++) {
+    console.log(coding[i]);
+}
+
+
+
+
+
+
